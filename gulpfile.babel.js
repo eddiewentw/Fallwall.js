24a690642abe10ff85c77a1091ccc2617d0acc04
Eddie Wen <eddiewen.tw@gmail.com>
2017-02-05 12:31:13 +0800

    Add sourcemap for .min.js
    install gulp-sourcemaps to make a sourcemap file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,14 +2,17 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
+import sourcemap from 'gulp-sourcemaps';
 
 gulp.task('minify', () => (
 	gulp.src('./src/fallwall.js')
+		.pipe( sourcemap.init() )
 		.pipe( babel() )
 		.pipe( uglify({
 			preserveComments: 'license',
 		}) )
 		.pipe( rename('fallwall.min.js') )
+		.pipe( sourcemap.write('.') )
 		.pipe( gulp.dest('./dist') )
 ));
 
